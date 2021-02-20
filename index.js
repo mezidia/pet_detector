b57aa69ff493801899390bcd4b3adb3ce6163c8f
liza <gorbunova.elizaveta@gmail.com>
2021-02-20 18:51:57 +0200

    trying to host

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const Database = require('./scripts/node_js/database').Database;
 
 const database = new Database('cXiZf1YUZTNtMrX8');
 
-const server = new Server(5000, database);
+const server = new Server(process.env.PORT || 5000, database);
 
