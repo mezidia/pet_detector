576e2c3f46a00b0838b412f64cd05a98613e5dd0
La7rodectus <maxmega27@gmail.com>
2021-02-20 14:45:30 +0200

    add grecaptcha

--- scripts/front_js/script.js
+++ scripts/front_js/script.js
@@ -2,8 +2,13 @@
 
 //import smt from './modules.js';
 
-
-
-
-
-
+function checkRecaptcha() {
+  const response = grecaptcha.getResponse();
+  if(response.length == 0) { 
+    alert("no pass"); 
+  }
+  else { 
+    //reCaptch verified
+    alert("pass"); 
+  }
+}
