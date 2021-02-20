df5f8f4c4db1fc9add35431ca907fc1f5aad4908
La7rodectus <maxmega27@gmail.com>
2021-02-21 00:21:49 +0200

    fix time on card

--- scripts/front_js/views/mainPage.js
+++ scripts/front_js/views/mainPage.js
@@ -2,22 +2,31 @@
 
 const display = (data) => {
   console.log(data);
+  function getNormalTimeStr(timeStr) {
+    const time = new Date(+timeStr);
+    console.log(time);
+    const theyear = time.getFullYear();
+    const themonth = time.getMonth() + 1;
+    const thetoday = time.getDate();
+    return `${thetoday}/${themonth}/${theyear}`;
+  }
   if (data === null || Object.entries(data).length === 0) return '';
+  console.log(+data.data[0].date);
   return `
    <div style="height: 50px"></div>
   <h1>${data.status === 'lost' ? 'Загублені улюбленці' : 'Знайдені улюбленці'}</h1>
   <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 style="max-width: 100%">
     ${data.data.map(pet => `<div class="card" style="min-width: 100px">
-      <img class="card-img-top" src="${pet.image}" alt="Card image cap">
+      <img class="card-img-top" src="${pet.image}" alt="Фотографія тварини">
       <div class="card-body">
-        <h5 class="card-title">Вид ${pet.animal}</h5>
-        <h6>${pet.breed}</h6>
-        <h6>${pet.color}</h6>
-        <h6>${pet.description}</h6>
+        <h5 class="card-title">Вид - ${pet.animal}</h5>
+        ${pet.breed ? `<h6>Порода тварини - ${pet.breed} </h6>` : ''}
+        <h6>Колір тварини - ${pet.color}</h6>
+        ${pet.description ? `<h6>Опис тварини - ${pet.description} </h6>` : ''}
         <button type="button" class="infobtn btn btn-primary" id="${pet.id}">Info</button>
       </div>
       <div class="card-footer">
-        <small class="text-muted">Опрелюднено: ${pet.date}</small>
+        <small class="text-muted">Опрелюднено: ${getNormalTimeStr(pet.date)}</small>
       </div>
     </div>`).join('\n')}
   </div>
