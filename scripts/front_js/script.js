6d4354104c79ad6ca3b34d3944eafef180b462e1
gazinaft <gazinaft@gmail.com>
2021-02-21 03:37:27 +0200

    color blind features

--- scripts/front_js/script.js
+++ scripts/front_js/script.js
@@ -76,12 +76,26 @@ const newLost = async (evt) => {
 
 }
 
+const colorBlind = (color= true) => () => {
+  if(color) {
+    document.querySelector('*').style.filter = 'grayscale(100%)';
+  } else {
+    document.querySelector('*').style.filter = 'grayscale(0)';
+  }
+  color = !color;
+}
+const color = colorBlind();
 document.addEventListener('click', (evt) => {
   if (evt.target.id === 'lost-submit') newLost();
   if (evt.target.id === 'found-submit') newFound();
   if (evt.target.id === 'found-assign') changeHash('foundForm');
   if (evt.target.id === 'lost-assign') changeHash('lostForm');
   if (evt.target.className.split(' ')[0] === 'infobtn') changeHash(`${evt.target.className.split(' ').pop()}/${evt.target.id}`);
+  if (evt.target.id === 'colorblind') color();
+});
+
+document.addEventListener('input', (evt) => {
+  if (evt.target.id === 'hue') document.querySelector('body').style.filter = `hue-rotate(${evt.target.value}deg)`
 });
 
 async function loadMain() {
